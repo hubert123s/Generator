63fe0b3002179b0edc176cf5353639e141e6e2cf
hubert123 <hubert.antoniszyn98@gmail.com>
2022-10-25 21:03:16 +0200

    added docs

--- src/main/java/com/example/generator/GeneratorService.java
+++ src/main/java/com/example/generator/GeneratorService.java
@@ -16,6 +16,12 @@ class GeneratorService {
     private final GeneratorRepository generatorRepository;
     private final GeneratorSystem generatorSystem;
 
+    /**
+     * The function checks if the min and max values are valid, if the selected characters are valid, and if the number of
+     * strings is valid. If all of these are valid, the function saves the parameters to the database
+     *
+     * @param generatorParameters the object that contains all the parameters of the generator.
+     */
     public void saveFileAndGenerate(GeneratorParameters generatorParameters) {
         int charsSize = generatorSystem.selectedCharacters(generatorParameters.getChars()).size();
         if (generatorParameters.getMin() > generatorParameters.getMax()) {
@@ -29,10 +35,21 @@ class GeneratorService {
         }
     }
 
+    /**
+     * Find all generators that are active.
+     *
+     * @return A list of GeneratorParameters objects
+     */
     public List<GeneratorParameters> findActiveGenerator() {
         return generatorRepository.findAllByStatus(Status.ACTIVE);
     }
 
+    /**
+     * This function reads a file from the file system and returns a list of strings.
+     *
+     * @param id The id of the file to be read
+     * @return A list of strings.
+     */
     public List<String> findFileById(Long id) {
        return generatorSystem.readFile(id);
     }
--- src/main/java/com/example/generator/GeneratorSystem.java
+++ src/main/java/com/example/generator/GeneratorSystem.java
@@ -21,7 +21,8 @@ public class GeneratorSystem {
 
     private final Random random = new Random();
     private final GeneratorRepository generatorRepository;
-
+    // A method that is called every 100 milliseconds. It checks if there is a generator that is waiting to be generated.
+    // If there is, it sets the status to active, and then generates all the strings and saves them to a file.
     @Async
     @Scheduled(fixedRate = 100)
     public void generateAll() {
@@ -36,6 +37,12 @@ public class GeneratorSystem {
         }
     }
 
+    /**
+     * It takes a set of strings and a generator parameters object, and writes the strings to a file
+     *
+     * @param stringSet The set of strings to be written to the file.
+     * @param generatorParameters the parameters that the user has entered in the GUI.
+     */
     public void saveFile(Set<String> stringSet, GeneratorParameters generatorParameters) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("id" + generatorParameters.getId() + ".txt", true))) {
             for (String singleString : stringSet) {
@@ -46,6 +53,12 @@ public class GeneratorSystem {
             e.printStackTrace();
         }
     }
+    /**
+     * It reads a file with a given id and returns a list of strings
+     *
+     * @param id the id of the file to be read
+     * @return List of Strings
+     */
     public List<String> readFile(Long id) {
         List<String> listToShow =new ArrayList<>();
         if (generatorRepository.existsById(id)) {
@@ -64,6 +77,12 @@ public class GeneratorSystem {
         }
         return listToShow;
     }
+    /**
+     * It generates a set of strings with the given parameters
+     *
+     * @param generatorParameters This is the object that contains all the parameters that the user has entered.
+     * @return A set of strings.
+     */
     public Set<String> generateAllString(GeneratorParameters generatorParameters) {
         HashSet<String> stringSet = new HashSet<>();
         List<String> distinctString = selectedCharacters(generatorParameters.getChars());
@@ -74,6 +93,13 @@ public class GeneratorSystem {
         return stringSet;
     }
 
+    /**
+     * It generates a random string of random length, with random characters from a list of distinct characters
+     *
+     * @param distinctString A list of strings that are distinct from each other.
+     * @param generatorParameters This is the object that contains the parameters for the generator.
+     * @return A random string of characters.
+     */
     public String generateString(List<String> distinctString, GeneratorParameters generatorParameters) {
         int length = random.nextInt(generatorParameters.getMin(), generatorParameters.getMax());
         int[] numbers = random.ints(0, generatorParameters.getMax()).distinct().limit(length).toArray();
@@ -84,6 +110,12 @@ public class GeneratorSystem {
         return newString.toString();
     }
 
+    /**
+     * "Return a list of distinct characters from the input string, but only if the character is a single character."
+     *
+     * @param chars a string of characters
+     * @return A list of distinct characters that are one character long.
+     */
     public List<String> selectedCharacters(String chars) {
         return Arrays.stream(chars.trim().split(","))
                 .distinct()
@@ -91,6 +123,13 @@ public class GeneratorSystem {
                 .toList();
     }
 
+    /**
+     * Given a maximum number of characters and a character set, calculate the maximum number of possible combinations.
+     *
+     * @param max The maximum number of characters to generate.
+     * @param charsSize The size of the character set you're using.
+     * @return The maximum number of characters that can be generated.
+     */
     public int calculateMaxCharsGenerated(int max, int charsSize) {
         return (int) Math.pow(charsSize, max);
     }
